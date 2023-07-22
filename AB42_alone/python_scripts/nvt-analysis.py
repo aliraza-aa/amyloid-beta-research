39b205f4fbd97fcba8381fceda96024b6a14320d
zcbtar9 <zcbtar9@login01.kathleen.ucl.ac.uk>
2023-07-22 18:49:49 +0100

    performed NVT equilibration of AB42 confirmations

--- AB42_alone/python_scripts/nvt-analysis.py
+++ AB42_alone/python_scripts/nvt-analysis.py
@@ -2,8 +2,7 @@ import subprocess
 
 for i in range(48):
 
-    p = subprocess.Popen(f"gmx_mpi_d energy -f system/simulations/confirmation{i}/nvt.edr -o system/simulations/confirmation{i}/nvt_temperature.xvg",
-                         stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    p = subprocess.Popen(f"gmx_mpi_d energy -f ../system/simulations/confirmation{i}/nvt.edr -o ../system/simulations/confirmation{i}/nvt_temperature.xvg", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     out, err = p.communicate(input="16 0\n".encode())
     if p.returncode != 0:
         print(out)
