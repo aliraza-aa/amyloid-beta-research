44a86bb66e1937008f9961efec9bfdaa3a198e4e
unknown <alirp366@gmail.com>
2023-07-24 22:28:39 +0500

    changed a python script

--- AB42_alone/system/simulations/production_run/housekeeping.py
+++ AB42_alone/system/simulations/production_run/housekeeping.py
@@ -3,8 +3,10 @@ import subprocess
 for i in range(2, 48):
     # subprocess.run(f"mkdir simulations/confirmation_{i}", shell=True)
     subprocess.run(
-        f"mv ../confirmation{i}/confout.part0001.gro ../confirmation{i}/em.gro", shell=True)
+        f"rm ../confirmation{i}/production_run.part0001.edr", shell=True)
     subprocess.run(
-        f"mv ../confirmation{i}/ener.part0001.edr ../confirmation{i}/em.edr", shell=True)
+        f"rm ../confirmation{i}/production_run.part0001.log", shell=True)
     subprocess.run(
-        f"mv ../confirmation{i}/md.part0001.log ../confirmation{i}/em.log", shell=True)
+        f"rm ../confirmation{i}/production_run.part0001.trr", shell=True)
+    subprocess.run(
+        f"rm ../confirmation{i}/production_run.part0001.xtc", shell=True)
