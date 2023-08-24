588f2aca2c6544044e17567b3f2f73fa0d1cf25f
unknown <alirp366@gmail.com>
2023-08-24 20:28:24 +0500

    initializing test 4

--- AB42_WM/python_scripts/production_run-prep.py
+++ AB42_WM/python_scripts/production_run-prep.py
@@ -1,9 +1,9 @@
 import subprocess
 
-for i in range(1, 48):
-    # subprocess.run(f"mkdir simulations/confirmation_{i}", shell=True)
+for i in range(0):
+    # subprocess.run(f"mkdir simulations_test4/confirmation_{i}", shell=True)
     subprocess.run(
-        f"gmx_mpi_d grompp -f ../system/mdp_files/production_run.mdp -c ../system/simulations/replica{i}/npt_output.gro -p ../system/simulations/replica{i}/topolm.top -o ../system/simulations/replica{i}/production_run_input.tpr -maxwarn 1", shell=True)
+        f"gmx_mpi_d grompp -f ../system/mdp_files/run.mdp -c ../system/simulations_test4_test4/replica{i}/npt_output.gro -p ../system/simulations_test4/replica{i}/topolm.top -o ../system/simulations_test4/replica{i}/production_run_input.tpr -maxwarn 1", shell=True)
 
 
-# gmx_mpi_d grompp -f ../system/mdp_files/production_run.mdp -c ../system/simulations/replica0/npt_output.gro -r ../system/simulations/replica0/npt_output.gro -p ../system/topol.top -o ../system/simulations/confirmation0/production_run_input.tpr
+# gmx_mpi_d grompp -f ../system/mdp_files/production_run.mdp -c ../system/simulations_test4/replica0/npt_output.gro -r ../system/simulations_test4/replica0/npt_output.gro -p ../system/topol.top -o ../system/simulations_test4/confirmation0/production_run_input.tpr
---
+++ AB42_WM/system/simulations_test4/production_run/production_run.sh
@@ -0,0 +1,50 @@
+#RUNFILE FOR KATHLEEN
+
+#!/bin/bash --l
+
+#$ -S /bin/bash
+
+#$ -l h_rt=7:00:0
+
+#$ -l mem=1G
+
+#$ -N AB42_WM-production-test4-1.1
+
+#$ -pe mpi 240
+
+#$ -cwd
+
+# send email to me
+#$ -M zcbtar9@ucl.ac.uk
+
+WORKDIR=`pwd`
+
+
+
+ 
+module load beta-modules
+module unload -f compilers mpi gcc-libs
+module load gcc-libs/10.2.0
+module load cmake
+module load compilers/gnu/10.2.0
+module load numactl/2.0.12
+module load binutils/2.36.1/gnu-10.2.0
+module load ucx/1.9.0/gnu-10.2.0
+module load mpi/openmpi/4.0.5/gnu-10.2.0
+module load python3/3.9-gnu-10.2.0
+module load libmatheval
+module load flex
+module use ~/modulefiles
+module load plumed-2.9.0-gnu-10.2.0
+module load gromacs-2022.5-plumed-2.9.0
+
+# export PLUMED_NUM_THREADS=$OMP_NUM_THREADS
+
+ 
+
+cd $WORKDIR
+ 
+
+#RESTART
+
+gerun gmx_mpi_d mdrun -s production_run_input.tpr -multidir ../replica{0..47} -plumed ../../../plumed/plumed.dat -o production_run.trr -x production_run.xtc -c production_run_output.gro -g production_run.log -e production_run.edr -v -noappend -cpt 60 -cpnum -maxh 6 &> production_run1.log
