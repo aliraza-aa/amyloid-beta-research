29179e3f2013c68540197bfecaf343dd23b5d7ba
unknown <alirp366@gmail.com>
2023-08-28 14:14:15 +0500

    changed python script

--- AB42_WM/python_scripts/production_run-prep.py
+++ AB42_WM/python_scripts/production_run-prep.py
@@ -1,9 +1,9 @@
 import subprocess
 
-for i in range(1, 48):
+for i in range(0):
     # subprocess.run(f"mkdir simulations_test4/confirmation_{i}", shell=True)
     subprocess.run(
-        f"gmx_mpi_d grompp -f ../system/mdp_files/run.mdp -c ../system/simulations_test4/replica{i}/npt_output.gro -p ../system/simulations/replica{i}/topolm.top -o ../system/simulations_test4/replica{i}/production_run_input.tpr -maxwarn 1", shell=True)
+        f"gmx_mpi grompp -f ../system/mdp_files/run.mdp -c ../system/simulations_test4/replica{i}/npt_output.gro -p ../system/simulations/replica{i}/topolm.top -o ../system/simulations_test9/replica{i}/production_run_input.tpr -maxwarn 1", shell=True)
 
 
 # gmx_mpi_d grompp -f ../system/mdp_files/production_run.mdp -c ../system/simulations_test4/replica0/npt_output.gro -r ../system/simulations_test4/replica0/npt_output.gro -p ../system/topol.top -o ../system/simulations_test4/confirmation0/production_run_input.tpr
