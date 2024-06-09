6f97452ca31d8741443cd64ecb1cb13292d7e0f9
Ali Raza <alirp366@gmail.com>
2024-06-09 14:07:46 +0100

    updating state after 600 K simulation of collapsed AB40 peptide

---
+++ ab40/system/setup/energy_min.sh
@@ -0,0 +1,45 @@
+#RUNFILE FOR KATHLEEN
+
+#!/bin/bash --l
+
+#$ -S /bin/bash
+
+#$ -l h_rt=12:00:0
+
+#$ -l mem=1G
+
+#$ -N AB40-initial-em
+
+#$ -pe mpi 80
+
+#$ -cwd
+
+#$ -m bes
+
+# send email to me
+#$ -M zcbtar9@ucl.ac.uk
+
+WORKDIR=`pwd`
+
+ 
+module unload -f compilers mpi gcc-libs
+module load beta-modules
+module load gcc-libs/10.2.0
+module load compilers/gnu/10.2.0
+module load mpi/openmpi/4.0.5/gnu-10.2.0
+module load openblas/0.3.13-serial/gnu-10.2.0
+module load python3/3.9-gnu-10.2.0
+module load libmatheval
+module load flex
+module use ~/modulefiles
+module load plumed-2.9.0-gnu-10.2.0
+module load gromacs-2022.5-plumed-2.9.0-sp
+
+# export PLUMED_NUM_THREADS=$OMP_NUM_THREADS
+
+ 
+
+cd $WORKDIR
+
+
+gerun gmx_mpi mdrun -s em -v -maxh 10 &> em_terminal.log
