82ed389176d5fc262a6dc31aa3b07f6f08f51e8d
Ali Raza <alirp366@gmail.com>
2024-06-19 20:06:58 +0100

    comm replica structures obtained from npt equilibration

--- ab40/system/production/6-nvt-278-js.sh
+++ ab40/system/production/6-nvt-278-js.sh
@@ -8,9 +8,9 @@
 
 #$ -l mem=1G
 
-#$ -N AB40-production-em
+#$ -N AB40-production-nvt
 
-#$ -pe mpi 80
+#$ -pe mpi 160
 
 #$ -cwd
 
@@ -42,4 +42,4 @@ module load gromacs-2022.5-plumed-2.9.0-sp
 cd $WORKDIR
 
 
-gerun gmx_mpi mdrun -multidir r{0..39} -deffnm em -v -maxh 11 &> em_terminal.log
+gerun gmx_mpi mdrun -multidir r{0..39} -deffnm nvt -v -maxh 11 &> 6-nvt-278-terminal.log
