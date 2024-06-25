71910aa658f1cf82153c80303d07e6f64698d161
unknown <alirp366@gmail.com>
2024-06-25 15:12:55 +0100

    adding production_run jobscripts

---
+++ ab40/system/production/production_run.sh
@@ -0,0 +1,43 @@
+#RUNFILE FOR KATHLEEN
+
+#!/bin/bash --l
+
+#$ -S /bin/bash
+
+#$ -l h_rt=2:00:0
+
+#$ -l mem=1G
+
+#$ -pe mpi 400
+
+#$ -N AB42_WM-production-test11-1.1
+
+#$ -cwd
+
+
+WORKDIR=`pwd`
+
+ 
+module unload -f compilers mpi gcc-libs
+module load beta-modules
+module load gcc-libs/10.2.0
+module load compilers/gnu/10.2.0
+module load mpi/openmpi/4.0.5/gnu-10.2.0
+module load openblas/0.3.13-serial/gnu-10.2.0
+module load python3/3.9-gnu-10.2.0
+module load libmatheval
+module load flex
+module use ~/modulefiles
+module load plumed-2.9.0-gnu-10.2.0
+module load gromacs-2022.5-plumed-2.9.0-sp
+
+# export PLUMED_NUM_THREADS=$OMP_NUM_THREADS
+
+ 
+
+cd $WORKDIR
+ 
+
+#RESTART
+
+gerun gmx_mpi mdrun -multidir ../r{0..39} -plumed ../../plumed/plumed.dat -deffnm production -v -noappend -cpt 60 -cpnum &> production-run.log
