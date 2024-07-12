d063ef4b308d42b7b1cc629fbd804461a8b7e24e
unknown <alirp366@gmail.com>
2024-07-12 12:10:55 +0100

    added ab42 dir, modified production_run.mdp: changed dt to 4 fs

---
+++ ab42/load_gromacs_plumed.sh
@@ -0,0 +1,12 @@
+module unload -f compilers mpi gcc-libs
+module load beta-modules
+module load gcc-libs/10.2.0
+module load compilers/gnu/10.2.0
+module load mpi/openmpi/4.0.5/gnu-10.2.0
+module load openblas/0.3.13-serial/gnu-10.2.0
+module load python3/3.9-gnu-10.2.0
+module load libmatheval
+module load flex
+module use ~/modulefiles
+module load plumed-2.9.0-gnu-10.2.0
+module load gromacs-2022.5-plumed-2.9.0-sp
